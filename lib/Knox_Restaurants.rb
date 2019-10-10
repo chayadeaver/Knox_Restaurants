79203d708ffc3708e21d1f5eedbc999d31336cf3
Elias <eliasiscoding@gmail.com>
2019-10-10 15:31:19 -0400

    making minor changes

--- lib/Knox_Restaurants.rb
+++ lib/Knox_Restaurants.rb
@@ -1,4 +1,6 @@
 require "Knox_Restaurants/version"
+require "Knox_Restaurants/cli"
+require "Knox_Restaurants/restaurant"
 
 module KnoxRestaurants
   class Error < StandardError; end
--- lib/Knox_Restaurants/restaurant.rb
+++ lib/Knox_Restaurants/restaurant.rb
@@ -1,5 +1,3 @@
 class KnoxRestaurant::Restaurant 
     attr_accessor :name, :cuisine, :address, :website
-
-
 end
