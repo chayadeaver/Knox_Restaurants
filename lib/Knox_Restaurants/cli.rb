9d219607a4cc0470397317dfc82848ae724ee830
Chaya Deaver <chayadeaver@gmail.com>
2019-10-20 16:05:12 -0400

    working on flow

--- lib/Knox_Restaurants/cli.rb
+++ lib/Knox_Restaurants/cli.rb
@@ -3,9 +3,14 @@ class KnoxRestaurants::CLI
     def call
         puts "Welcome to Knoxville!"
         puts "What are you in the mood for?"
+        puts "Enter 'end' to exit"
         KnoxRestaurants::Scraper.fetch_data
         puts KnoxRestaurants::Restaurant.get_cuisines
         
+        input = gets.chomp.downcase
+
+        while 
+
         
     end
 
