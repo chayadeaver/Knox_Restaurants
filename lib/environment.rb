ba4b83cb09476e6869c5e3c15238a94a08460eb1
chayadeaver <chayadeaver@gmail.com>
2020-07-28 22:49:04 -0400

    Removed Pry

--- lib/environment.rb
+++ lib/environment.rb
@@ -3,7 +3,6 @@ require_relative "./knox_restaurants/version"
 require_relative "./knox_restaurants/cli"
 require_relative "./knox_restaurants/restaurant"
 require_relative "./knox_restaurants/api"
-require "pry"
 require "httparty"
 require "dotenv/load"
 
